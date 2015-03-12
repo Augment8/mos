31d3ead515eadeca95124dabff79ee1512bd4907
Tomohiko Himura <eiel.hal@gmail.com>
2015-03-12 12:57:16 +0900

    インデント修正

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ var WebSocketServer = require("websocket").server;
 var http = require("http");
 
 var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
+  console.log((new Date()) + ' Received request for ' + request.url);
+  response.writeHead(404);
+  response.end();
 });
 server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
+  console.log((new Date()) + ' Server is listening on port 8080');
 });
 
 var wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
+  httpServer: server,
+  // You should not use autoAcceptConnections for production
+  // applications, as it defeats all standard cross-origin protection
+  // facilities built into the protocol and the browser.  You should
+  // *always* verify the connection's origin and decide whether or not
+  // to accept it.
+  autoAcceptConnections: false
 });
 
 function originIsAllowed(origin) {
@@ -84,7 +84,7 @@ wsServer.on("request", function(request) {
           session_id = cookie.value;
         }
       }
-console.log(sessions);
+      console.log(sessions);
       if (session_id in sessions) {
         sendSession(sessions[session_id]);
       } else {
